8aa9c780179a63fbaed58fcde286eacbbbfb7d5b
JerryMillar124 <148289242+JerryMillar124@users.noreply.github.com>
2024-04-24 23:54:03 -0600

    Nuevo

--- mi-proyecto-react/src/App.js
+++ mi-proyecto-react/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import './componentes/Diseño.css'; 
+import './componentes/Diseño.css';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient("https://zfqlwupplahepakjglsp.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmcWx3dXBwbGFoZXBha2pnbHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMjU0NzIsImV4cCI6MjAyODkwMTQ3Mn0.ym4G8npHB33qCUKwhy2KXO_gu-8-LT1UcNLSLlW7kfw");
+
 
 function App() {
+
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    getCategorias();
+  }, []);
+
+  async function getCategorias() {
+    const { data } = await supabase.from("categorias").select();
+    setCategorias(data);
+  }
+
   return (
+
     <div className="contenedor">
+      <ul>
+        {categorias.map((categorias) => (
+          <li key={categorias.nombre}>{categorias.nombre}</li>
+        ))}
+      </ul>
       <h1 className="titulo">Diseño de Aplicacion Web</h1>
       <ul>
         <li><button className="BtnDir"><a href="categoria.html">Categoria</a></button></li>
